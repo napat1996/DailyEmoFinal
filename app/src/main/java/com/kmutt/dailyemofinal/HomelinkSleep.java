6e007b4289eb42587c44e44c8515952986b0a58b
benvrt <thasvarin.jc@hotmail.com>
2018-09-17 16:15:14 +0700

    (ADD) textdhome ka

--- app/src/main/java/com/kmutt/dailyemofinal/HomelinkSleep.java
+++ app/src/main/java/com/kmutt/dailyemofinal/HomelinkSleep.java
@@ -3,6 +3,9 @@ package com.kmutt.dailyemofinal;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.widget.RelativeLayout;
 
 import com.github.mikephil.charting.charts.PieChart;
 
@@ -18,5 +21,18 @@ public class HomelinkSleep extends AppCompatActivity {
 
         mChart = (PieChart)findViewById(R.id.chart1);
         mChart.setBackgroundColor(Color.WHITE);
+
+
+    }
+    private void moveOffscreen(){
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int height = metrics.heightPixels;
+        int offset = (int)(height*0.5);
+
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)mChart.getLayoutParams();
+        params.setMargins(0,0,0,-offset);
+        mChart.setLayoutParams(params);
     }
 }
