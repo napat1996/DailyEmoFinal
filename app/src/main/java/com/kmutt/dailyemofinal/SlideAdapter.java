f45079c23abdee95bdf26d2c65638f74113db458
benvrt <thasvarin.jc@hotmail.com>
2018-09-25 18:27:49 +0700

    [ADD] suggestion_slide

---
+++ app/src/main/java/com/kmutt/dailyemofinal/SlideAdapter.java
@@ -0,0 +1,94 @@
+package com.kmutt.dailyemofinal;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SlideAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater inflater;
+
+// list of images
+    public int [] lst_image ={
+            R.drawable.icon_moon,
+            R.drawable.icon_heart,
+            R.drawable.icon_step,
+
+    };
+// list of titles
+    public String[] lst_title ={
+         "Sleep",
+         "Heart rate",
+         "Step"
+
+    };
+
+    // list of description
+    public String[] lst_des = {
+
+            "Des1",
+            "Des2",
+            "Des3"
+
+    };
+
+    public int[] lst_bg = {
+
+            Color.rgb(213, 197, 198),
+            Color.rgb(213, 197, 198),
+            Color.rgb(213, 197, 198)
+
+    };
+
+
+
+  public SlideAdapter (Context context){
+      this.context = context;
+  }
+
+
+    @Override
+    public int getCount() {
+        return lst_title.length;
+    }
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return (view==(LinearLayout)object);
+    }
+
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+       // return super.instantiateItem(container, position);
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slide_one,container,false);
+        LinearLayout layoutslide =  (LinearLayout) view.findViewById(R.id.slidelinearlayout);
+        ImageView imgslide = (ImageView) view.findViewById(R.id.slideimg);
+        TextView txttitle = (TextView) view.findViewById(R.id.txttitle);
+        TextView txtdes = (TextView) view.findViewById(R.id.txtdescription);
+        layoutslide.setBackgroundColor(lst_bg[position]);
+        imgslide.setImageResource(lst_image[position]);
+        txttitle.setText(lst_title[position]);
+        txtdes.setText(lst_des[position]);
+        container.addView(view);
+        return view;
+
+    }
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+
+}
